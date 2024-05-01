94f715952892ceb405f4e9b27c96d9c52843c4a1
junsoo186 <junsoo186@gmail.com>
2024-05-01 15:01:26 +0900

    thread 예제

---
+++ src/basic/ch23/Mother.java
@@ -0,0 +1,15 @@
+package basic.ch23;
+
+public class Mother extends Thread {
+
+	BankAccount account;
+	public Mother( BankAccount account) {
+		this.account = account;
+	}
+	@Override
+	public void run() {
+		account.withDraw(5_000);
+	
+	
+	}
+}
